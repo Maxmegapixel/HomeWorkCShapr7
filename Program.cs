f44337584b9a602cd37a136ebfd5237a330b8da9
Maksim <maxmegapixel@mail.ru>
2023-08-24 22:51:16 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -1,42 +1,47 @@
 ﻿// Урок 7. Двумерные массивы
-// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+// возвращает значение этого элемента или же указание, что такого элемента нет.
 
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// позиция i = 4, j = 2 -> такой позиции нет
+// позиция i = 1, j = 0 -> 5
 
 
-PrintInput("Введите количество строк массива: ");
+PrintMess("Введите количество строк массива: ");
 int rows = GetInput();
-PrintInput("Введите количество столбцов массива: ");
+PrintMess("Введите количество столбцов массива: ");
 int columns = GetInput();
-PrintArray(GetArray(rows, columns, -10, 10));
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+FindElement(array);
 
 int GetInput()
 {
-    return  int.Parse(Console.ReadLine()!);
+    return int.Parse(Console.ReadLine()!);
 }
-void PrintInput(string a)
+void PrintMess(string a)
 {
     Console.WriteLine(a);
 }
 
-double[,] GetArray(int m, int n, int minValue, int maxValue)
+int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-    double[,] result = new double[m, n];
-    Random rand = new Random();
+    int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = rand.NextDouble() * rand.Next(minValue, maxValue + 1);
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
     return result;
 }
 
-void PrintArray(double[,] array)
+void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -47,3 +52,14 @@ void PrintArray(double[,] array)
         Console.WriteLine();
     }
 }
+
+void FindElement(int[,] array)
+{
+    PrintMess("Введите номер строки искомого элемента: ");
+    int i = GetInput();
+    PrintMess("Введите номер столбца искомого элемента: ");
+    int j = GetInput();
+    if (i >= array.GetLength(0) || i < 0 || j < 0 || j >= array.GetLength(1)) PrintMess("такого элемента нет!");
+    else Console.WriteLine($"Значение элемента: {array[i, j]}");
+}
+
